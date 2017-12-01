077010cc5f0cfe25f005f6f04ad1454527c90ffb
Ian <ianangillis@gmail.com>
2017-12-01 09:34:58 +0100

    opdracht 2 in de les

---
+++ H1/src/opdracht2/Klas.java
@@ -0,0 +1,46 @@
+package opdracht2;
+
+public class Klas {
+	
+	private String klasNaam;
+	private int aantalStudenten;
+	public static int MAXSTUDENTEN;
+	public static int count = 0;
+	
+	{
+		MAXSTUDENTEN = 40;
+		count++;
+	}
+	
+	Klas(){
+		this("1TINx", 0);
+	}
+	
+	Klas(String nieuwKlasNaam, int nieuwAantalStudenten){
+		
+		this.setKlasNaam(nieuwKlasNaam);
+		this.setAantalStudenten(nieuwAantalStudenten);
+	}
+	
+	public String getKlasNaam() {
+		return this.klasNaam;
+	}
+	
+	public int getAantalStudent() {
+		return this.aantalStudenten;
+	}
+	
+	public void setKlasNaam(String nieuwKlasNaam){
+		this.klasNaam = nieuwKlasNaam;
+	}
+	
+	public void setAantalStudenten(int nieuwAantalStudenten) {
+		if(nieuwAantalStudenten > Klas.MAXSTUDENTEN) {
+			nieuwAantalStudenten = Klas.MAXSTUDENTEN;
+		}
+		this.aantalStudenten = nieuwAantalStudenten;
+	}
+	
+	
+	
+}
